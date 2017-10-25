45e7b1e9b66a90888d59b152bd78db48514ee1b4
Santhosh Gugan <santhoshg1990@gmail.com>
2017-10-26 01:43:12 +0530

    adding fontawesome

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,26 @@
 // Custom Javascript
+
+// Santhosh
+
+var headerScroll = function(vrtOff){
+	if(vrtOff > 20){
+		$('#header').addClass('transBg');
+	}else{
+		$('#header').removeClass('transBg');
+	}	
+};
+
+$(document).ready(function(){
+
+	/*ON load check the scroll position*/
+	headerScroll(window.pageYOffset);
+
+	$(document).scroll(function(event) {
+		/* Act on the event */
+		headerScroll(window.pageYOffset);
+
+	});
+});
+
+
+// Stephen
